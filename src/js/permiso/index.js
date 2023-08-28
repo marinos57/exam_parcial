18f863341c175eafdb251cf1ad0be45c5ca58d45
marino5710 <brianmarin4@gmail.com>
2023-08-27 21:48:12 -0600

    se agregaron las columnas y los botones de la datatable

--- src/js/permiso/index.js
+++ src/js/permiso/index.js
@@ -29,3 +29,46 @@ const datatable = new Datatable('#tablaClientes', {
             render : () => contador ++
             
         },
+        {
+            title : 'USUARIO',
+            data: 'permiso_usuario'
+        },
+        {
+            title : 'PERMISO',
+            data: 'permiso_rol',
+        },
+        {
+            title : 'ESTADO',
+            data: 'usu_estado',
+        },
+        {
+            title : 'MODIFICAR',
+            data: 'permiso_id',
+            searchable : false,
+            orderable : false,
+            render : (data, type, row, meta) => `<button class="btn btn-warning" data-id='${data}' data-usuario='${row["permiso_usuario"]}' data-rol='${row["permiso_rol"]}' >Modificar</button>`
+        },
+        {
+            title : 'ELIMINAR',
+            data: 'permiso_id',
+            searchable : false,
+            orderable : false,
+            render : (data, type, row, meta) => `<button class="btn btn-danger" data-id='${data}' >Eliminar</button>`
+        },
+        {
+            title : 'ACTIVAR',
+            data: 'usu_id',
+            searchable : false,
+            orderable : false,
+            render : (data, type, row, meta) => row['usu_estado'].trim()==='PENDIENTE' || row['usu_estado'].trim()==='INACTIVO'? `<button class="btn btn-success" data-id='${data}' >Activar</button>` :''
+        },
+        {
+            title : 'DESACTIVAR',
+            data: 'usu_id',
+            searchable : false,
+            orderable : false,
+            render : (data, type, row, meta) => row['usu_estado'].trim()==='ACTIVO'? `<button class="btn btn-info" data-id='${data}' >DESACTIVAR</button>`: ''
+        },
+    ]
+})
+
