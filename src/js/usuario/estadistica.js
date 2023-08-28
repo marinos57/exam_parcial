78f733c52e6cc3010fd238873314ce2610bbc67a
marino5710 <brianmarin4@gmail.com>
2023-08-27 23:14:59 -0600

    se creo el js de estadistica

---
+++ src/js/usuario/estadistica.js
@@ -0,0 +1,77 @@
+
+import Chart from "chart.js/auto";
+import { Toast } from "../funciones";
+
+const canvas = document.getElementById('chartUsuarios')
+const btnActualizar = document.getElementById('btnActualizar')
+const context = canvas.getContext('2d');
+
+
+const chartVentas = new Chart(context, {
+    type : 'bar',
+    data : {
+        labels : [],
+        datasets : [
+            {
+                label : 'Usuarios',
+                data : [],
+                backgroundColor : []
+            },
+        
+        ]
+    },
+    options : {
+        indexAxis : 'x'
+    }
+})
+
+const getEstadisticas = async () => {
+    const url = `/exam_parcial/API/usuarios/estadisticaUsu`;
+    const config = {
+        method : 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config)
+        const data = await respuesta.json();
+
+        chartVentas.data.labels = [];
+        chartVentas.data.datasets[0].data = [];
+        chartVentas.data.datasets[0].backgroundColor = []
+
+
+
+        if(data){
+
+            data.forEach( registro => {
+                chartVentas.data.labels.push(registro.usu_estado)
+                chartVentas.data.datasets[0].data.push(registro.cantidad_usuarios)
+                chartVentas.data.datasets[0].backgroundColor.push(getRandomColor())
+            });
+
+        }else{
+            Toast.fire({
+                title : 'No se encontraron registros',
+                icon : 'info'
+            })
+        }
+        
+        chartVentas.update();
+       
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getRandomColor = () => {
+    const r = Math.floor( Math.random() * 256)
+    const g = Math.floor( Math.random() * 256)
+    const b = Math.floor( Math.random() * 256)
+
+    const rgbColor = `rgba(${r},${g},${b},0.5)`
+    return rgbColor
+}
+
+getEstadisticas();
+
+btnActualizar.addEventListener('click', getEstadisticas )
