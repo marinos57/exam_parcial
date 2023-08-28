77fa62083c14b3b8c3b5665445bdfa79e050da16
marino5710 <brianmarin4@gmail.com>
2023-08-27 20:48:27 -0600

    se creo la funcion buscarAPI

--- controllers/RolController.php
+++ controllers/RolController.php
@@ -74,20 +74,53 @@ class RolController{
     }
 
     public static function eliminarAPI(){
+        try{
+            $rol_id = $_POST['rol_id'];
+            $rol = Rol::find($rol_id);
+            $rol->rol_situacion = 0;
+            $resultado = $rol->actualizar();
 
 
-        
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje'=>"Se ha eliminado el registro con éxito.",
+                    'codigo' => 1,
+                    ] );
+            }else{
+                echo json_encode( [
+                    'mensaje'=>"El rol no pudo ser borrado. Por favor intenta nuevamente o contacta al administrador del sistema.",
+                    'codigo' => 0
+            ]);
+            }
+            }catch(Exception $e){
+                echo json_encode([
+                    'detalle' => $e->getMessage(),
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+            ]);
+         }
     }
 
+    public static function buscarAPI(){
+        $rol_nombre = $_GET['rol_nombre'] ?? '';
+        $sql = "SELECT * FROM rol WHERE rol_situacion = 1 ";
+        if ($rol_nombre != '') {
+            $rol_nombre = strtolower($rol_nombre);
+            $sql .= " AND LOWER(rol_nombre) LIKE '%$rol_nombre%' ";
+    }
 
+    try{
+        $roles = Rol::fetchArray($sql);
+        echo json_encode($roles);
 
+    } catch (Exception $e) {
+        echo json_encode([
+            'detalle' => $e->getMessage(),
+            'mensaje' => 'Ocurrió un error',
+            'codigo' => 0
+        ]); 
+     }
 
-
-
-
-
-
-
-
+    }
 
 }
