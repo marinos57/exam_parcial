29186e381922ea4dc0db6601a22fe64c4612bcb1
marino5710 <brianmarin4@gmail.com>
2023-08-27 20:26:00 -0600

    se creo la clase rol

--- models/Rol.php
+++ models/Rol.php
@@ -3,3 +3,27 @@
 namespace Model;
 
 class Rol extends ActiveRecord{
+    public static $tabla = 'rol';
+    public static $columnasDB = ['rol_nombre', 'rol_situacion'];
+    public static $idTabla = 'rol_id';
+
+    public $rol_id;
+    public $rol_nombre;
+    public $rol_situacion;
+    public function __construct($args =[]){
+
+        $this->rol_id = $args['rol_id'] ?? null;
+        $this->rol_nombre = $args['rol_nombre'] ?? '';
+        $this->rol_s = $args['rol_nombre'] ?? '';
+
+
+        
+
+
+
+    }
+
+
+
+
+     }
