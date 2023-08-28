e42c8c11048950f7f92fa211fd8e4f54c61e603b
marino5710 <brianmarin4@gmail.com>
2023-08-27 20:32:57 -0600

    se termino de crear la funcion guardarAPI en el controller ROL

--- controllers/RolController.php
+++ controllers/RolController.php
@@ -13,18 +13,36 @@ class RolController{
 
         try {
             //code...
-        } catch (\Throwable $th) {
+            $nombre = $_POST["rol_nombre"];
+            $rol = new Rol([
+                'rol_nombre'=>$nombre]);
+            $resultado = $rol->crear();
+        
+            if ($resultado['resultado'] == 1) {
+                echo json_encode([
+                    'mensaje' => 'Registro guardado correctamente',
+                    'codigo' => 1
+
+                ]);
+             }else{
+                echo json_encode([
+                    'mensaje' => 'Ocurrió un error',
+                    'codigo' => 0
+                ]);
+             }
+
+        } catch (Exception $e) {
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+
             //throw $th;
         }
-
-
-
-
-
-
-
     }
 
+    
 
 
 
