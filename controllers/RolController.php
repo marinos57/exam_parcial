1f751621169bfc146606c8176101c4a2ef5d0d31
marino5710 <brianmarin4@gmail.com>
2023-08-27 20:29:36 -0600

    se creara la clase guardarAPI en este controlador, si en un futuro es escalable la aplicacion se dejara declaradas las funciones, por el momento los roles se insertaran de forma manual en la base de datos con insert into rol

---
+++ controllers/RolController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Rol;
+use MVC\Router;
+
+class RolController{
+
+    public static function guardarAPI(){
+
+
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
