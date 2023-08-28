9ffbebc4345a330f922f6cb26f6a72ca1e550ee1
marino5710 <brianmarin4@gmail.com>
2023-08-27 19:35:30 -0600

    se creo el controlador PermisoController con la funcion que renderiza hacia la vista de permisos

---
+++ controllers/PermisoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers;
+
+use Exception;
+use Model\Permiso;
+use Model\Usuario;
+use Model\Rol;
+use MVC\Router;
+
+class PermisoController
+{
+    public static function index(Router $router){
+        $usuarios = static::buscarUsuario();
+        $roles = static::buscarRol();
+        $permisos = Permiso::all();
+
+        $router->render('permisos/index', [
+            'usuarios' => $usuarios,
+            'roles' => $roles,
+            'permisos' => $permisos,
+        ]);
+    }
+
+
+}
