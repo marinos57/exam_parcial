960c28aca9e345b44d91a9cd4511bffaac3b4750
marino5710 <brianmarin4@gmail.com>
2023-08-27 23:01:49 -0600

    se creo la vista de estadistica para mostrar los usuarios que se encuentran activos e inactivos

---
+++ views/usuarios/estadistica.php
@@ -0,0 +1,8 @@
+<h1>GRAFICA PARA MOSTRAR LOS USUARIOS QUE SE ENCUENTRAN ACTIVOS E INACTIVOS</h1>
+<button id="btnActualizar" class="btn btn-info">Actualizar</button>
+<div class="row">
+    <div class="col-lg-7">
+        <canvas id="chartUsuarios" width="100%"></canvas>
+    </div>
+</div>
+<script src="<?=asset('./build/js/usuario/estadistica.js') ?>"></script>
